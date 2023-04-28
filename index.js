5c998cb2c55f7597eaf75f4783ed897d9de6e23b
sereja <kornikov.sergey@hexly.ru>
2023-04-28 17:59:54 +0300

    two step

--- index.js
+++ index.js
@@ -15,5 +15,30 @@ const content = fs.readFileSync(path.join(
 ), 'utf-8');
 
 // BEGIN
-console.log(content)
+
+console.log(content.split('\r\n'))
+
+const rows = content.split('\r\n')
+const data = rows.slice(1)
+console.log(data.length)
+
+
+const  cells = data.map(row => row.slice(2, row.length - 2).split(' | '));//отфильтровываем строки и в каждой ...
+console.log(cells)
+
+
+const priceForOne = (list) => Number(list[list.length-1]);//цена найма -последний элемент который мы перевели в числовой типа данных
+  const countOfSU = 10;//число сильных юнитов
+  const countOfSSU = 20;// число вторых по силе 
+  const maxStrength = Math.max(...cells.map((el) => el[1]));//здесь мы вычислил наибольший показатель под индексом 1
+  const sUnit = cells.filter((el) => el[1] === String(maxStrength)).flat();//
+  const sSUnits =cells.filter((el) => el[1] !== String(maxStrength));
+  const maxStrengthSSU = Math.max(...sSUnits.map((el) => el[1]));
+  const sSUnit = sSUnits.filter((el) => el[1] === String(maxStrengthSSU)).flat();
+  const price = priceForOne(sUnit) * countOfSU + priceForOne(sSUnit) * countOfSSU;
+  console.log(`Цена найма 10 самых сильных существ и 20 существ вторых по силе: ${Math.round(price)}`);
+
 // END
+//.reduce((acc, el) => { acc += el; return acc; }, 0)   - 0 числовой аккум
+//.reduce((acc, el) => {acc += el; return acc; }, '')   - '' строковый аккум. Строки так же складываются
+//.reduce((acc, el) => {*что-то делаем с el *; return [...acc, *тут константа - результат действий с el*]; }, [])   - []  аккум - массив. Строки так же складываются
